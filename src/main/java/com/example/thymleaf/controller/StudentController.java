4370e719ad2750218356de5960dd007cc23ba5d4
Gitanjali_21 <paul.geetanjali2311@gmail.com>
2022-05-16 17:27:36 +0530

    Added Thymleaf

--- src/main/java/com/example/thymleaf/controller/StudentController.java
+++ src/main/java/com/example/thymleaf/controller/StudentController.java
@@ -7,7 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class StudentController {
     private StudentServiceInterface studentRepository;
 
